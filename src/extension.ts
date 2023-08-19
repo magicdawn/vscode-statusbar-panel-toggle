86d1f96ecb77ef95383e1a6074a0835fbfd2246b
Tao <magicdawn@qq.com>
2023-08-20 02:08:59 +0800

    chore: clean up

--- src/extension.ts
+++ src/extension.ts
@@ -2,20 +2,15 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
 
-let statusBarItem: vscode.StatusBarItem
-
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(ctx: vscode.ExtensionContext) {
-  console.log('111 panel-toggle')
-
   // create a new status bar item that we can now manage
-  statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, -100)
-  statusBarItem.command = 'workbench.action.togglePanel'
-  statusBarItem.text = '$(layout-panel) Panel'
-  statusBarItem.show()
-
-  ctx.subscriptions.push(statusBarItem)
+  const barItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, -100)
+  barItem.command = 'workbench.action.togglePanel'
+  barItem.text = '$(layout-panel) Panel'
+  barItem.show()
+  ctx.subscriptions.push(barItem)
 }
 
 // This method is called when your extension is deactivated
